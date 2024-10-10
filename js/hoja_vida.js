6442d010dcfd04d6f2ce63a48a8b9fae69a881af
Cami10R <j.camilo_roa@hotmail.com>
2024-10-09 21:04:08 -0500

    agreando new branch

---
+++ js/hoja_vida.js
@@ -0,0 +1,9 @@
+
+
+const btnclick = document.getElementById("btnAddEdu");
+
+    btnclick.addEventListener('click', registrar());
+
+     function registrar() {
+        alert ("Educacion Registrada")
+     }
